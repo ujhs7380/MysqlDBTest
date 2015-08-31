e07b89ca3576352556bbf90ba041c7501c99fdba
ujhs7380 <ujhs7380@naver.com>
2015-08-31 21:22:30 +0900

    eunjinmysqltest

---
+++ DB.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+
+namespace MysqlDBTest
+{
+	public static class DB
+	{
+		static string _connectionString;
+		static MySqlConnection Conn; 
+		static DB()
+		{
+			
+			MySqlConnectionStringBuilder connBuilder = new MySqlConnectionStringBuilder();
+			connBuilder.Add("Database", "Project_JJK");
+			connBuilder.Add("Data Source", "hellojkw.com");
+			connBuilder.Add("User Id", "uj7380");
+			connBuilder.Add("Password", "uj7380");
+
+			_connectionString = connBuilder.ToString();
+			Conn = new MySqlConnection(_connectionString);
+		}
+		
+
+		public static void Addtable(string tablename)
+		{
+			int a=0;
+			try
+			{
+				Conn.Open();
+				string create = string.Format("CREATE TABLE {0} (myId INT PRIMARY KEY, myName CHAR(50))", tablename);
+				MySqlCommand cmd = new MySqlCommand(create, Conn);
+				cmd.Parameters.AddWithValue("@tablename", tablename);
+				cmd.ExecuteNonQuery();
+				Console.WriteLine("success for create + {0}", tablename);
+				Console.ReadLine();
+				Console.WriteLine("press enter key to continue ");
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				Conn.Close();
+			}
+		}
+
+		  public static void Insertdata(int id,string name)
+		{
+		try	  
+		{
+			Conn.Open();
+			string insertsql = string.Format("INSERT INTO eunjin VALUES (@id, @name)",id, name);
+			MySqlCommand myCommand = new MySqlCommand(insertsql,Conn);
+			myCommand.Parameters.AddWithValue("@id", id);
+			myCommand.Parameters.AddWithValue("@name", name);
+			myCommand.ExecuteNonQuery();
+		  }
+			  catch(Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				Conn.Close();
+			}
+		}
+
+		
+		  public static void Checktablelist()
+		  {
+			try	  
+				{
+				
+					Conn.Open();
+					string showcm = string.Format("SHOW TABLES");
+					MySqlCommand showtable = new MySqlCommand(showcm, Conn);
+					MySqlDataReader Reader;
+					Reader = showtable.ExecuteReader();
+				while (Reader.Read())
+						{
+							int i = 0;
+							string row;
+							row=Reader.GetValue(0).ToString();
+							
+				    //row = Reader.GetValue(i).ToString();
+							Console.WriteLine(row);
+							i++;				  
+						}
+				}
+			catch(Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+			finally
+				{	
+					Conn.Close();
+				}
+			
+}
+
+
+		}
+
+	}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace MysqlDBTest
+{
+	
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			//DB Connection check
+			//string tablenamelist;
+			//for checking table list in Project_JJK database.
+			Console.WriteLine("we will check table list using show tables command");
+			Console.ReadLine();
+			DB.Checktablelist();
+			
+			//creating table
+			Console.Write("Please enter tablename to create table,excepting table list :");
+			string tablename = Console.ReadLine();
+			Console.WriteLine(tablename);
+			DB.Addtable(tablename);
+
+
+			
+			Console.Write("please enter id :");
+			string idvalue = Console.ReadLine();
+			int Number = int.Parse(idvalue);
+			Console.Write("please enter name :");
+			string namevalue = Console.ReadLine();
+			DB.Insertdata(Number, namevalue);
+			Console.ReadLine();
+
+						
+	
+		}
+	}
+}
+
+			/*
+			 * 0. 테이블 만든다.
+			 * 1. 사용자의 입력을 받아. Console.ReadLine() 검색해봐
+			 * 2. 값을 DB 에 쓴다.
+			 * 3. 다시 1번으로 반복
+			 * */
